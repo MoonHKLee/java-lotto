aa4bf3460920e066b526362683ccd937e74a40d0
MoonHKLee <mf311110@gmail.com>
2023-05-18 08:56:17 +0900

    test(lotto) : 로또객체 생성 테스트코드 작성. - 테스트코드 추가.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,7 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoTest {
 
     @Test
-    void createLotto() {
+    @DisplayName("로또는 중복된 번호가 없다.")
+    void createLotto_duplicate() {
         //given
         Lotto lotto = new Lotto();
 
@@ -19,5 +21,33 @@ public class LottoTest {
 
         //then
         assertThat(numbers).doesNotHaveDuplicates();
+    }
+
+    @Test
+    @DisplayName("로또는 1~45의 번호를 갖는다.")
+    void createLotto_range() {
+        //given
+        Lotto lotto = new Lotto();
+
+        //when
+        List<Integer> numbers = lotto.getNumbers();
+
+        //then
+        assertThat(numbers)
+                .allMatch(number -> number >= 1)
+                .allMatch(number -> number <= 45);
+    }
+
+    @Test
+    @DisplayName("로또는 6개의 번호를 갖는다.")
+    void createLotto_count() {
+        //given
+        Lotto lotto = new Lotto();
+
+        //when
+        List<Integer> numbers = lotto.getNumbers();
+
+        //then
+        assertThat(numbers).hasSize(6);
     }
 }
