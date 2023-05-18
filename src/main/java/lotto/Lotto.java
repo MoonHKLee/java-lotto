19fb9548d718d337b7b5fa9d5584e42ca07673c0
MoonHKLee <mf311110@gmail.com>
2023-05-18 16:57:47 +0900

    feat(lotto) : 로또객체 생성 구현 완료.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+    private List<Integer> numbers = new ArrayList<>();
+
+    public Lotto() {
+        for (int i = 1; i <= 45; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        numbers = numbers.subList(0, Math.min(numbers.size(), 6));
+        Collections.sort(numbers);
+    }
+
     public List<Integer> getNumbers() {
-        return null;
+        return numbers;
     }
 }
